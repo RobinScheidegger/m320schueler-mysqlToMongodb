70cca4df9700dc5f19566ef317fe4a69afbebe02
Robin Scheidegger <robin.scheidegger@swisscom.com>
2023-04-01 13:20:18 +0200

    feat(change handling if klassen is empty)

--- main.go
+++ main.go
@@ -14,37 +14,49 @@ import (
 )
 
 func main() {
-	// connect to MySQL DB
+	// Daten von MySql DB holen
+	schuelerArray, klasseArray, klasseSchuelerArray := dbMysql()
+
+	// Schüler Objekte erstellen
+	var allSchuelers []map[string]interface{} = createSchueler(schuelerArray, klasseArray, klasseSchuelerArray)
+
+	// Daten in MongoDb einfügen
+	mongoDb(allSchuelers)
+}
+
+// Diese Funktion returnt alle gefundenen Daten in drei Doppelarray a
+func dbMysql() ([][]string, [][]string, [][]int) {
+	// mit DB connecten
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/m320schueler")
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return nil, nil, nil
 	}
 	defer db.Close()
 
-	// Abfrage MySQL DB
+	// Daten holen
 	schueler, err := db.Query("SELECT * FROM schueler")
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return nil, nil, nil
 	}
 	defer schueler.Close()
 
 	klasse, err := db.Query("SELECT * FROM klasse")
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return nil, nil, nil
 	}
 	defer klasse.Close()
 
 	klasseSchueler, err := db.Query("SELECT * FROM klasse_schueler")
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return nil, nil, nil
 	}
 	defer klasseSchueler.Close()
 
-	//fill in Array
+	//Daten in Arrays abfüllen
 	var schuelerArray [][]string
 	for schueler.Next() {
 		var id int
@@ -52,7 +64,7 @@ func main() {
 		err := schueler.Scan(&id, &benutzername, &name, &vorname)
 		if err != nil {
 			fmt.Println(err.Error())
-			return
+			return nil, nil, nil
 		}
 		singleSchueler := []string{strconv.Itoa(id), benutzername, name, vorname}
 		schuelerArray = append(schuelerArray, singleSchueler)
@@ -65,7 +77,7 @@ func main() {
 		err := klasse.Scan(&kid, &name)
 		if err != nil {
 			fmt.Println(err.Error())
-			return
+			return nil, nil, nil
 		}
 		singleKlasse := []string{strconv.Itoa(kid), name}
 		klasseArray = append(klasseArray, singleKlasse)
@@ -77,42 +89,18 @@ func main() {
 		err := klasseSchueler.Scan(&kid, &id)
 		if err != nil {
 			fmt.Println(err.Error())
-			return
+			return nil, nil, nil
 		}
 		singleKlasseSchueler := []int{kid, id}
 		klasseSchuelerArray = append(klasseSchuelerArray, singleKlasseSchueler)
 	}
-
-	// Schüler Objekte erstellen
-	var allSchuelers []map[string]interface{} = createSchueler(schuelerArray, klasseArray, klasseSchuelerArray)
-
-	// Konfigurieren Sie den MongoDB-Client
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer client.Disconnect(context.Background())
-
-	// Wählen Sie die Datenbank und die Sammlung aus
-	database := client.Database("m165schueler")
-	collection := database.Collection("schueler")
-
-	// Fügen Sie Daten in die Sammlung ein
-	for _, schueler := range allSchuelers {
-		data := bson.M(schueler)
-		result, err := collection.InsertOne(context.Background(), data)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		fmt.Println(result.InsertedID)
-	}
+	return schuelerArray, klasseArray, klasseSchuelerArray
 }
 
+// Diese Funktion returnt einen Array mit allen Schülern als Map
 func createSchueler(schuelerArray [][]string, klasseArray [][]string, klasseSchuelerArray [][]int) []map[string]interface{} {
 	var allSchuelers []map[string]interface{}
+	// iteriert durch jeden Schüler & füllt daten in Array
 	for _, schueler := range schuelerArray {
 		var benutzername string = schueler[1]
 		var name string = schueler[2]
@@ -122,8 +110,10 @@ func createSchueler(schuelerArray [][]string, klasseArray [][]string, klasseSchu
 		if err != nil {
 			panic(err)
 		}
+		// holt alle Klassen des schülers
 		var klassen []string = getKlassen(id, klasseSchuelerArray, klasseArray)
 
+		// Map für ein Schüler erstellen
 		schuelerMap := make(map[string]interface{})
 		schuelerMap["benutzername"] = benutzername
 		schuelerMap["name"] = name
@@ -135,14 +125,17 @@ func createSchueler(schuelerArray [][]string, klasseArray [][]string, klasseSchu
 	return allSchuelers
 }
 
+// Diese Funktion returnt einen Array mit allen Klassen eines Spezifischen Schülers
 func getKlassen(id int, klasseSchuelerArray [][]int, klasseArray [][]string) []string {
 	var klassenIds []int
 	var klassenNames []string
+	// findet alle klassen ids die der Schüler id zugewiesen sind
 	for _, ids := range klasseSchuelerArray {
 		if id == ids[1] {
 			klassenIds = append(klassenIds, ids[0])
 		}
 	}
+	// für jede id in klassenIds wird der klassenname in klassenNames gespeichert
 	for _, id := range klassenIds {
 		for _, klassen := range klasseArray {
 			klassenId, err := strconv.Atoi(klassen[0])
@@ -155,4 +148,38 @@ func getKlassen(id int, klasseSchuelerArray [][]int, klasseArray [][]string) []s
 		}
 	}
 	return klassenNames
+}
+
+// Diese Funktion fügt jeden schüler in die MongoDB ein
+func mongoDb(allSchuelers []map[string]interface{}) {
+	// mit MongoDB connecten
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer client.Disconnect(context.Background())
+
+	// datenbank & collection wählen
+	database := client.Database("m165schueler")
+	collection := database.Collection("schueler")
+
+	// für jeden Schüler wird ein BSON datei erstellt & in DB inserted
+	for _, schueler := range allSchuelers {
+		var data bson.M
+		if len(schueler["klassen"].([]string)) > 0 {
+			data = bson.M(schueler)
+		} else {
+			delete(schueler, "klassen")
+			data = bson.M(schueler)
+			fmt.Println(data)
+		}
+		result, err := collection.InsertOne(context.Background(), data)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(result.InsertedID)
+	}
 }
